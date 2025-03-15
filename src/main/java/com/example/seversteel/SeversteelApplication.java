753e57b69fb3f168b80b8238c7b432fa8d3e91c1
Daniil Chernousov <valerij.tehnik@yandex.ru>
2025-03-15 18:56:39 +0300

    init commit

---
+++ src/main/java/com/example/seversteel/SeversteelApplication.java
@@ -0,0 +1,13 @@
+package com.example.seversteel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeversteelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeversteelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/seversteel/SeversteelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seversteel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeversteelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
